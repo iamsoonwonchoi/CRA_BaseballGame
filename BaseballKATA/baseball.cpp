df120d2b78d40e19218ab8f6899a78e9324f0a1c
soonwonchoi <soonwon.choi@samsung.com>
2025-07-02 13:23:14 +0900

    [refactoring] page 3

--- BaseballKATA/baseball.cpp
+++ BaseballKATA/baseball.cpp
@@ -8,24 +8,38 @@ class Baseball
 public:
 	void guess(const string& guessNumber)
 	{
+		assertIllegalArgument(guessNumber);
+	}
+
+private:
+	void assertIllegalArgument(const string& guessNumber)
+	{
 		if (guessNumber.length() != 3)
 		{
 			throw std::length_error("Must be three letters.");
 		}
-		
+
 		for (char ch : guessNumber)
 		{
-			if (ch < '0' || ch > '9')
-			{
-				throw std::invalid_argument("Must be number.");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw std::invalid_argument("Must be number.");
 		}
 
-		if ((guessNumber[0] == guessNumber[1])
-			|| (guessNumber[0] == guessNumber[2])
-			|| (guessNumber[1] == guessNumber[2]))
+		if (isDuplicatedNumber(guessNumber))
 		{
 			throw std::invalid_argument("Must not have the same number");
 		}
 	}
+
+	bool isDuplicatedNumber(const string& guessNumber)
+	{
+		if ((guessNumber[0] == guessNumber[1])
+			|| (guessNumber[0] == guessNumber[2])
+			|| (guessNumber[1] == guessNumber[2]))
+		{
+			return true;
+		}
+		return false;
+	}
+
 };
